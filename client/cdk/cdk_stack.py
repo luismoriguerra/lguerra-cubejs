e3f85b90d32361fcb4e34cde09d70c7299f187ea
Luis Mori Guerra <luismorith@gmail.com>
2023-02-13 20:36:26 -0500

    feat: adding client
    Signed-off-by: Luis Mori Guerra <luismorith@gmail.com>

---
+++ client/cdk/cdk_stack.py
@@ -0,0 +1,52 @@
+from aws_cdk import (
+    # Duration,
+    Stack,
+    aws_s3 as s3,
+    aws_cloudfront as cf,
+    aws_s3_deployment as s3_deployment,
+    # aws_sqs as sqs,
+    CfnOutput,
+    RemovalPolicy
+)
+from constructs import Construct
+
+
+class CdkStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # The code that defines your stack goes here
+        app_bucket = s3.Bucket(
+            self, "angular-app-bucket",
+            website_index_document="index.html",
+            website_error_document="index.html",
+            public_read_access=True,
+            removal_policy=RemovalPolicy.DESTROY,
+        )
+
+        cf_dist = cf.CloudFrontWebDistribution(
+            self, "angular-cloudfront",
+            origin_configs=[
+                cf.SourceConfiguration(
+                    s3_origin_source=cf.S3OriginConfig(
+                        s3_bucket_source=app_bucket),
+                    behaviors=[cf.Behavior(is_default_behavior=True)])])
+
+        s3_deployment.BucketDeployment(
+            self, "angular-app-bucket-deployment",
+            sources=[s3_deployment.Source.asset(
+                path="../dist/client")],
+            destination_bucket=app_bucket,
+            distribution=cf_dist,
+            distribution_paths=["/*"]
+        )
+
+        CfnOutput(
+            self, "angular-cloudfront-url",
+            value=cf_dist.distribution_domain_name)
+        # example resource
+        # queue = sqs.Queue(
+        #     self, "CdkQueue",
+        #     visibility_timeout=Duration.seconds(300),
+        # )
---
+++ cubejs/readme.md
@@ -0,0 +1,8 @@
+
+yarn start
+
+
+http://localhost:4000/#/build?query={%22measures%22:[%22Gitarchive.user_name%22],%22order%22:{%22Gitarchive.user_name%22:%22desc%22},%22dimensions%22:[%22Gitarchive.username%22]}
+
+https://obliged-mockingbird.aws-eu-central-1.cubecloudapp.dev/dev-mode/dev-luismoriguerra-9aafb402/cubejs-api/v1
+
