cc3fde207665e69fa22cf6c9e212040ecc976149
Luis Mori Guerra <luismorith@gmail.com>
2023-02-14 21:24:29 -0500

    chore: clean up
    Signed-off-by: Luis Mori Guerra <luismorith@gmail.com>

--- cubejs/schema/GithubEvents.js
+++ cubejs/schema/GithubEvents.js
@@ -1,90 +1,96 @@
 cube(`GithubEvents`, {
   sql: `SELECT * FROM public.github_events`,
-  preAggregations: {
-    // Pre-Aggregations definitions go here
+  preAggregations: {// Pre-Aggregations definitions go here
     // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started
+
+    main: {
+      measures: [GithubEvents.count],
+      dimensions: [GithubEvents.repoName],
+      timeDimension: GithubEvents.createdAt,
+      granularity: `day`
+    }
   },
   joins: {},
   measures: {
     count: {
       type: `count`,
-      drillMembers: [id, repoName, commitId, createdAt, prOrIssueCreatedAt],
+      drillMembers: [id, repoName, commitId, createdAt, prOrIssueCreatedAt]
     },
     number: {
       sql: `number`,
-      type: `sum`,
-    },
+      type: `sum`
+    }
   },
   dimensions: {
     id: {
       sql: `id`,
       type: `number`,
-      primaryKey: true,
+      primaryKey: true
     },
     prMerged: {
       sql: `pr_merged`,
-      type: `string`,
+      type: `string`
     },
     orgLogin: {
       sql: `org_login`,
-      type: `string`,
+      type: `string`
     },
     state: {
       sql: `state`,
-      type: `string`,
+      type: `string`
     },
     creatorUserLogin: {
       sql: `creator_user_login`,
-      type: `string`,
+      type: `string`
     },
     type: {
       sql: `type`,
-      type: `string`,
+      type: `string`
     },
     repoName: {
       sql: `repo_name`,
-      type: `string`,
+      type: `string`
     },
     actorLogin: {
       sql: `actor_login`,
-      type: `string`,
+      type: `string`
     },
     language: {
       sql: `language`,
-      type: `string`,
+      type: `string`
     },
     action: {
       sql: `action`,
-      type: `string`,
+      type: `string`
     },
     commitId: {
       sql: `commit_id`,
-      type: `string`,
+      type: `string`
     },
     createdAt: {
       sql: `created_at`,
-      type: `time`,
+      type: `time`
     },
     prOrIssueCreatedAt: {
       sql: `pr_or_issue_created_at`,
-      type: `time`,
+      type: `time`
     },
     closedAt: {
       sql: `closed_at`,
-      type: `time`,
+      type: `time`
     },
     prMergedAt: {
       sql: `pr_merged_at`,
-      type: `time`,
+      type: `time`
     },
     eventDay: {
       sql: `event_day`,
-      type: `time`,
+      type: `time`
     },
     eventMonth: {
       sql: `event_month`,
-      type: `time`,
-    },
+      type: `time`
+    }
   },
-  dataSource: `default`,
+  dataSource: `default`
 });
